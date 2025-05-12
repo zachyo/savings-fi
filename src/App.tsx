36367bd2ffe8d2961c44a0ac6f7047df9de36bba
zarkie <segunzacheusi@gmail.com>
2025-05-12 13:13:02 +0100

    chore: cleanups

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-// import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "../src/components/sonner";
-// import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NotFound from "./pages/NotFound";
@@ -12,22 +10,17 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    {/* <TooltipProvider> */}
-    {/* <Toaster /> */}
     <Sonner />
     <StudentsContextProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/register" element={<Register />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </StudentsContextProvider>
-    {/* </TooltipProvider> */}
   </QueryClientProvider>
-  // <div className="">Show</div>
 );
 
 export default App;
